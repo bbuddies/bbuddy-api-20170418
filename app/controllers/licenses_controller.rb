9d26106eb248adfb5e28ef197348ea624cc56a9e
cklin <cklin@kkstream.com.tw>
2017-04-18 17:40:09 +0800

    implement model and create license into database.

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,9 +1,18 @@
 
 class LicensesController < ApplicationController
   before_action :set_account, only: [:show, :update, :destroy]
-  before_action :authenticate_user!
 
   # POST /accounts
   def create
+    p params
+    license_params = params.require(:license).permit(:month, :amount)
+    p license_params
+    @license = License.new(license_params)
+
+    if @license.save
+      render json: @license, status: :created, location: @license
+    else
+      render json: errors(@license.errors), status: :unprocessable_entity
+    end
   end
 end
--- spec/controllers/licenses_controller_spec.rb
+++ spec/controllers/licenses_controller_spec.rb
@@ -3,7 +3,14 @@ require 'rails_helper'
 RSpec.describe LicensesController, type: :controller do
 
   it "should create licenses" do
-    post :create, licenses: { month: "2017-01", amount: 500 }
+    license = {
+        month: "2017-01", amount: 500
+    }
+    post :create, license: license, format: :json
+
+    actual = License.all.first
+    expect(actual.month).to eq(license[:month])
+    expect(actual.amount).to eq(license[:amount])
 
   end
 
