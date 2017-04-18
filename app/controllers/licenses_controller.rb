1504c5a59589eeb33d27e8b6b89bd59c40865822
JermySu <jermysu@kkstream.com.tw>
2017-04-18 17:34:59 +0800

    add response if success

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,5 +1,6 @@
 class LicensesController < ApplicationController
     def create
         License.create(params.require("license").permit(:month, :amount))
+        render :json => {:status => 'ok', :message => nil}
     end
 end
