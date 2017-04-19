6049d5f2db812bfd58558a96a7b3f1451a3e53f5
EggHsu <egghsu@kkstream.com.tw>
2017-04-19 10:35:16 +0800

    unit test of 'add exist license'

--- spec/controllers/licenses_controller_spec.rb
+++ spec/controllers/licenses_controller_spec.rb
@@ -1,30 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe LicensesController, type: :controller do
-  it 'add license' do
+  it 'add new license' do
     license = {month: '2017-05', amount: 100}
 
     post :create, license: license, format: :json
 
-    actual = License.all.first
-    expect(actual.month).to eq('2017-05')
+    actual = License.where(month: license[:month]).first
+    expect(!actual.nil?)
+    expect(actual.month).to eq(license[:month])
+    expect(actual.amount).to eq(100)
 
-
-      # license = License.new
-      # license.month = "2017.04"
-      # license.amount = 500
-      # license.add
-
-      # result = License.first
-      # expect(result.month).to eq "2017.04"
-      # expect(result.amount).to eq 500
-
-      #User.create!(email: 'joseph.yao.ruozhou@gmail.com', password: '123456', password_confirmation: '123456')
-
-    #string_changer = StringChanger.new
-
-    #reversed_string = string_changer.reverse_and_save('example string')
-
-    #expect(reversed_string).to eq 'gnirts elpmaxe'
   end
+
+  it 'add exist license depend on case 1' do
+    license = {month: '2017-05', amount: 100}
+
+    post :create, license: license, format: :json
+
+    license[:amount] = 200
+
+    post :create, license: license, format: :json
+
+    actual = License.where(month: license[:month]).first
+    
+    expect(!actual.nil?)
+    expect(actual.month).to eq(license[:month])
+    expect(actual.amount).to eq(200)
+  end
+  
 end
